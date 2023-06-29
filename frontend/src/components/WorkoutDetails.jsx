2494b35425fb17852fd1453af10ff635331aad13
Kevin Sidlacan <sidlacankevindale@gmail.com>
2023-06-29 21:36:27 +0800

    format date

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,12 +1,15 @@
 
 const WorkoutDetails = ({workout}) => {
   const {title, load, reps, createdAt} = workout
+  const date = new Date(createdAt)
+  const formattedDate = date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  
   return (
     <div className="workout-details">
       <h4>{title}</h4>
       <p><strong>Load(kg):</strong>{load}</p>
       <p><strong>Reps:</strong>{reps}</p>
-      <p>{createdAt}</p>
+      <p>{formattedDate}</p>
     </div>
   )
 }
