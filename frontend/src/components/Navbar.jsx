7fbb4fb4f77c130add5dc87b4f52eefdaab67f9a
Kevin Sidlacan <sidlacankevindale@gmail.com>
2023-06-30 16:01:24 +0800

    implement delete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Marmita's Workout</h1>
+          <h1>Marmita's Workout Plan</h1>
         </Link>
       </div>
     </header>
--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,15 +1,31 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
-const WorkoutDetails = ({workout}) => {
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
   const {title, load, reps, createdAt} = workout
   const date = new Date(createdAt)
   const formattedDate = date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-  
+
+  const handleClick = async () => {
+    const response = await fetch(`http://localhost:4000/api/workouts/${workout._id}`, {
+      method: 'DELETE'
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: data})
+    } else {
+      
+    }
+  }
   return (
     <div className="workout-details">
       <h4>{title}</h4>
-      <p><strong>Load(kg):</strong>{load}</p>
-      <p><strong>Reps:</strong>{reps}</p>
+      <p><strong>Load(kg): </strong>{load}</p>
+      <p><strong>Reps: </strong>{reps}</p>
       <p>{formattedDate}</p>
+      <span className='delete-button' onClick={ handleClick }>Delete</span>
     </div>
   )
 }
