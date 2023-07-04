053542602809169666b980826b1348d12e2460ed
Kevin Sidlacan <sidlacankevindale@gmail.com>
2023-07-04 18:37:11 +0800

    add login method in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 
 // signup user
@@ -8,7 +13,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password)
 
-    res.status(200).json({email, user})
+    // create token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
@@ -16,7 +24,18 @@ const signupUser = async (req, res) => {
 
 // login user
 const loginUser = async (req, res) => {
-  res.json( {message: 'login user'} )
+  const { email, password } = req.body
+try {
+  const user = await User.login(email,password)
+  // create token
+  const token = createToken(user._id)
+  res.json({email, token, message: 'Successfully Logged in'})
+} catch (error) {
+  res.status(400).json({error: error.message})
+}
+  
+  
+
 }
 
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -48,4 +48,27 @@ userSchema.statics.signup = async function (email, password) {
   return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password) {
+   // validation
+   if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne( { email } )
+
+  if (!user) {
+    throw Error('Incorrect email')
+  }
+
+  // match password
+  const matchedPassword = await bcrypt.compare(password, user.password)
+
+  if(!matchedPassword) {
+    throw Error('Incorrect password')
+  }
+
+  return user
+}
+
 module.exports = mongoose.model('User', userSchema)
