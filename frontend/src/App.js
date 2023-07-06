5075a4271d17a06f836770c4d86e3e4dfe8da82b
Kevin Sidlacan <sidlacankevindale@gmail.com>
2023-07-06 21:25:40 +0800

    add redirection when logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 //pages & components
-
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
+  const { user } = useAuthContext()
   return (
   <div className="App">
     <BrowserRouter>
@@ -15,15 +16,15 @@ function App() {
       <Routes>
         <Route 
         path="/"
-        element={<Home />}
+        element={user ? <Home /> : <Navigate to="/login"/> }
         />
         <Route 
         path="/login"
-        element={<Login />}
+        element={!user ? <Login /> : <Navigate to="/" />}
         />
         <Route 
         path="/signup"
-        element={<Signup />}
+        element={! user ? <Signup /> : <Navigate to="/" />}
         />
       </Routes>
     </div>
