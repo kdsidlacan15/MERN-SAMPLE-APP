55c6b0786d6c9a783e5f0ef7d7af0446f0423f72
Kevin Sidlacan <sidlacankevindale@gmail.com>
2023-08-23 01:19:04 +0800

    change app title

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Marmita's Workout Plan</h1>
+          <h1>My Workout Plan</h1>
         </Link>
         <nav>
           {user ? (
